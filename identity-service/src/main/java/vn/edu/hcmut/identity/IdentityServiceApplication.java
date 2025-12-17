7a30173b4e3230485272739880b2055c001a8165
KhoaLe2k4 <levodangkhoatg2@gmail.com>
2025-12-17 15:18:21 +0700

    refactor resource

--- identity-service/src/main/java/vn/edu/hcmut/identity/IdentityServiceApplication.java
+++ identity-service/src/main/java/vn/edu/hcmut/identity/IdentityServiceApplication.java
@@ -9,6 +9,5 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class IdentityServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(IdentityServiceApplication.class, args);
-        // }
     }
 }
