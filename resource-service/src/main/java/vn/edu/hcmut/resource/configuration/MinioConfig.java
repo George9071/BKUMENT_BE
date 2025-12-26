233a6efb719b50c3744a84cb750d2087d8d588e4
KhoaLe2k4 <levodangkhoatg2@gmail.com>
2025-12-26 09:24:52 +0700

    add resource service

---
+++ resource-service/src/main/java/vn/edu/hcmut/resource/configuration/MinioConfig.java
@@ -0,0 +1,21 @@
+package vn.edu.hcmut.resource.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.minio.MinioClient;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class MinioConfig {
+    private final MinioProperties minioProperties;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioProperties.getEndpoint())
+                .credentials(minioProperties.getAccessKey(), minioProperties.getSecretKey())
+                .build();
+    }
+}
