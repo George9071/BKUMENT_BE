a8b0102e67d20745d81c138a474af57f4f101a4d
PGeorge1709 <phucthanh0917@gmail.com>
2025-12-27 11:47:02 +0700

    add API docs for gateway

--- api-gateway/src/main/java/vn/edu/hcmut/gateway/configuration/AuthenticationFilter.java
+++ api-gateway/src/main/java/vn/edu/hcmut/gateway/configuration/AuthenticationFilter.java
@@ -5,13 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
@@ -20,6 +23,7 @@ import reactor.core.publisher.Mono;
 import vn.edu.hcmut.gateway.dto.response.APIResponse;
 import vn.edu.hcmut.gateway.service.IdentityService;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
@@ -31,20 +35,29 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     IdentityService identityService;
     ObjectMapper objectMapper;
 
+    @NonFinal
+    private String[] publicEndpoints = {
+            "/identity/auth/.*",
+            "/identity/accounts/registration",
+    };
+
+    @Value("${app.api-prefix}")
+    @NonFinal
+    private String apiPrefix;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("Enter authentication filter....");
 
         // CHECK IF PUBLIC ENDPOINT
-        String path = exchange.getRequest().getURI().getPath();
-        if (path.contains("/auth/**")) return chain.filter(exchange);
+        if (isPublicEndpoint(exchange.getRequest())) return chain.filter(exchange);
 
         // GET TOKEN FROM AUTHORIZATION HEADER
         List<String> authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
         if (CollectionUtils.isEmpty(authHeader))
             return unauthenticated(exchange.getResponse());
 
-        String token = authHeader.getFirst().replace("Bearer ", "");
+        String token = authHeader.get(0).replace("Bearer ", "");
         log.info("Token: {}", token);
 
         // VERIFY TOKEN WITH IDENTITY SERVICE
@@ -61,6 +74,15 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     @Override
     public int getOrder() {
         return -1;
+    }
+
+    private boolean isPublicEndpoint(ServerHttpRequest request){
+        String path = request.getURI().getPath();
+
+        if (path.contains("/v3/api-docs") || path.contains("/swagger-ui")) return true;
+
+        return Arrays.stream(publicEndpoints)
+                .anyMatch(s -> path.matches(apiPrefix + s));
     }
 
     // Helper method to write a 401 Unauthorized response to the client
