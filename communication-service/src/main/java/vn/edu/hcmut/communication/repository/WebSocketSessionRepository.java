c7611c19e9195a37236e2214a561f9ff0f3be1fc
PGeorge1709 <phucthanh0917@gmail.com>
2026-02-26 16:30:04 +0700

    communication-service: using web socket session (socketIO with netty) for real time chat.

---
+++ communication-service/src/main/java/vn/edu/hcmut/communication/repository/WebSocketSessionRepository.java
@@ -0,0 +1,16 @@
+package vn.edu.hcmut.communication.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import vn.edu.hcmut.communication.entity.WebSocketSession;
+
+import java.util.List;
+
+@Repository
+public interface WebSocketSessionRepository
+        extends MongoRepository<WebSocketSession, String> {
+
+    void deleteBySocketSessionId(String socketId);
+
+    List<WebSocketSession> findAllByUserIdIn(List<String> userIds);
+}
