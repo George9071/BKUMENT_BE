3134a4690448300d35878048d77492198bea9951
PGeorge1709 <phucthanh0917@gmail.com>
2026-03-18 15:56:05 +0700

    communication-service: package into jar

--- communication-service/src/main/java/vn/edu/hcmut/communication/configuration/OpenApiConfiguration.java
+++ communication-service/src/main/java/vn/edu/hcmut/communication/configuration/OpenApiConfiguration.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +17,10 @@ import java.util.List;
 public class OpenApiConfiguration {
     private static final String SECURITY_SCHEME_NAME = "bearerAuth";
 
-    // 1. Define the "Public" Group (Excludes internal paths)
+    @Value("${swagger.host-url:http://localhost:8888/api/v1/communication}")
+    private String gateway;
+
+    // the "public" group (excludes internal paths)
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
@@ -26,19 +30,26 @@ public class OpenApiConfiguration {
                 .build();
     }
 
-    // 2. Define the "Internal" Group (Shows everything)
+    // the "internal" group
     @Bean
     public GroupedOpenApi internalApi() {
-        return GroupedOpenApi.builder().group("internal").pathsToMatch("/**").build();
+        return GroupedOpenApi.builder()
+                .group("internal")
+                .pathsToMatch("/**")
+                .build();
     }
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("BKUMENT").version("1.0").description("APIs for communication-service"))
+                // --- SERVER CONFIGURATION---
                 .servers(List.of(
-                        new Server().url("http://localhost:8888/api/v1/communication").description("Gateway"),
-                        new Server().url("http://localhost:8083/communication").description("Local")))
+                        new Server().url(gateway).description("API Gateway (Default)"),
+                        new Server().url("http://localhost:8083/communication").description("Direct Local (Bypass Gateway)")
+                ))
+
+                // --- SECURITY CONFIGURATION---
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(new Components()
                         .addSecuritySchemes(
