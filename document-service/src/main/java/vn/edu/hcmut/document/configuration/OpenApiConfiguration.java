6f24595d5b917114e7c2753764aab7c5969d90db
KhoaLe2k4 <levodangkhoatg2@gmail.com>
2026-03-30 21:00:31 +0700

    deploy: apply swagger for document, blog, resource and social services

---
+++ document-service/src/main/java/vn/edu/hcmut/document/configuration/OpenApiConfiguration.java
@@ -0,0 +1,65 @@
+package vn.edu.hcmut.document.configuration;
+
+import java.util.List;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenApiConfiguration {
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Value("${swagger.gateway-url}")
+    private String gateway;
+
+    @Value("${swagger.host-url}")
+    private String self_host;
+
+    // the "public" group (excludes internal paths)
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .pathsToExclude("/**/internal/**")
+                .build();
+    }
+
+    // the "internal" group
+    @Bean
+    public GroupedOpenApi internalApi() {
+        return GroupedOpenApi.builder().group("internal").pathsToMatch("/**").build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("BKUMENT").version("1.0").description("APIs for Document-service"))
+                // --- SERVER CONFIGURATION---
+                .servers(List.of(
+                        new Server().url(gateway).description("API Gateway (Default)"),
+                        new Server().url(self_host).description("Direct Local (Bypass Gateway)")))
+
+                // --- SECURITY CONFIGURATION---
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT Authorization header using the Bearer scheme. "
+                                                + "Example: \"Authorization: Bearer {token}\"")));
+    }
+}
